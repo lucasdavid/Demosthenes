29b40e747b67d78e415b020a128c9e6e0f4c53d3
Lucas David <lucas.david@drexel.edu>
2015-02-02 16:11:26 -0200

    Add Validator factory

--- Demosthenes/Scripts/app/demosthenesApp.js
+++ Demosthenes/Scripts/app/demosthenesApp.js
@@ -1,3 +1,54 @@
 ﻿'use strict';
 
 var app = angular.module("demosthenesApp", ['ngRoute', 'ngResource']);
+
+app.factory('Validator', [function () {
+
+    return {
+        _data: null,
+        valid: function () {
+            return this._data && this._data.data && this._data.data.ModelState;
+        },
+        take: function (data) {
+            if (data) {
+                this._data = data;
+            }
+
+            return this;
+        },
+        errors: function () {
+            var result = [];
+
+            if (this.valid()) {
+                var modelState = this._data.data.ModelState;
+                for (var err in modelState) {
+                    result = result.concat(modelState[err]);
+                }
+            }
+
+            return result;
+        },
+        toastWarnings: function () {
+            var errors = this.errors();
+            for (var i = 0; i < errors.length; i++) {
+                toastr.warning(errors[i], 'Warning!');
+            }
+
+            return this;
+        },
+        toastErrors: function () {
+            if (this.valid()) {
+                for (var i = 0; i < this.errors().length; i++) {
+                    toastr.error(this.errors()[i], 'Error!');
+                }
+            }
+
+            return this;
+        },
+        otherwiseToastDefaultError: function () {
+            if (!this.valid()) {
+                toastr.error('Sorry, something went terribly wrong!', 'Error!');
+            }
+        }
+    };
+}]);
