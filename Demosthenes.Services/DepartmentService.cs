6776acab52b51b5e36865e28c7bde9d5f1731ccf
Lucas David <lucas.david@drexel.edu>
2015-02-03 13:36:41 -0200

    Add Demosthenes.Services

---
+++ Demosthenes.Services/DepartmentService.cs
@@ -0,0 +1,16 @@
+﻿using Demosthenes.Core;
+using Demosthenes.Data;
+using Demosthenes.Services.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demosthenes.Services
+{
+    public class DepartmentService : Service<Department>
+    {
+        public DepartmentService(DemosthenesContext db) : base(db) { }
+    }
+}
