df8c377712815173015953c0bfcf93f51e3ee610
Lucas David <lucas.david@drexel.edu>
2015-02-05 17:05:24 -0200

    Add DateAfterAttribute

---
+++ Demosthenes.Core/Attributes/DateAfterAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demosthenes.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TimeEqualsOrSmallerThanAttribute : ValidationAttribute
+    {
+        private string _property;
+        
+        public TimeEqualsOrSmallerThanAttribute(string property)
+        {
+            _property = property;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var first  = (TimeSpan)value;
+            var second = (TimeSpan)validationContext.ObjectType
+                .GetProperty(_property)
+                .GetValue(validationContext.ObjectInstance, null);
+
+            return first <= second
+                ? ValidationResult.Success
+                : new ValidationResult(ErrorMessage)
+                ;
+        }
+    }
+}
