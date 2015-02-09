ea03668526141990fccb54f2905496761f097958
Lucas David <lucas.david@drexel.edu>
2015-02-08 23:44:08 -0200

    Add Post model

--- Demosthenes.Core/ApplicationUser.cs
+++ Demosthenes.Core/ApplicationUser.cs
@@ -1,9 +1,9 @@
-﻿using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Demosthenes.Core
 {
@@ -22,5 +22,7 @@ namespace Demosthenes.Core
         [MinLength(2)]
         [MaxLength(255)]
         public string Name { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
     }
 }
---
+++ Demosthenes.Core/Post.cs
@@ -0,0 +1,27 @@
+﻿
+using Demosthenes.Core.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Demosthenes.Core
+{
+    public class Post : IDateTrackable
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(4), MaxLength(128)]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(8), MaxLength(1024)]
+        public string Content { get; set; }
+
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+        public virtual ApplicationUser Author { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+    }
+}
