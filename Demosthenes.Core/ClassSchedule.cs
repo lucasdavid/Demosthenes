55851db8d6e4491030f0db958ed749aae5885331
Lucas David <lucas.david@drexel.edu>
2015-02-11 13:06:01 -0200

    Reapply ClassSchedule constraint

--- Demosthenes.Core/ClassSchedule.cs
+++ Demosthenes.Core/ClassSchedule.cs
@@ -13,20 +13,20 @@ namespace Demosthenes.Core
         [Key, Column(Order = 1)]
         [Required]
         [ForeignKey("Schedule")]
-        //[Index("SCHEDULE_CLASSES_UNIQUE_IX", IsUnique = true, Order = 0)]
+        [Index("SCHCLS_UNIQUE_IX", IsUnique = true, Order = 1)]
         public int ScheduleId { get; set; }
         public virtual Schedule Schedule { get; set; }
 
         [Key, Column(Order = 2)]
         [Required]
         [ForeignKey("Class")]
-        //[Index("SCHEDULE_CLASSES_UNIQUE_IX", IsUnique = true, Order = 0)]
+        [Index("SCHCLS_UNIQUE_IX", IsUnique = true, Order = 2)]
         public int ClassId { get; set; }
         public virtual Class Class { get; set; }
 
         [Key, Column(Order = 3)]
         [Required]
-        //[Index("SCHEDULE_CLASSES_UNIQUE_IX", IsUnique = true, Order = 0)]
+        [Index("SCHCLS_UNIQUE_IX", IsUnique = true, Order = 3)]
         public DayOfWeek DayOfWeek { get; set; }
     }
 }
