9b62b8fbcb9fd65e3cd3ea652bbc61d3e14ea7de
Lucas David <lucas.david@drexel.edu>
2015-02-11 13:06:59 -0200

    Update ProfessorService

--- Demosthenes.Services/ProfessorService.cs
+++ Demosthenes.Services/ProfessorService.cs
@@ -1,11 +1,22 @@
 ﻿using Demosthenes.Core;
 using Demosthenes.Data;
 using Demosthenes.Services.Infrastructure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Demosthenes.Services
 {
     public class ProfessorService : Service<Professor>
     {
         public ProfessorService(DemosthenesContext db) : base(db) { }
+
+        public async Task<ICollection<Professor>> OfDepartment(int departmentId)
+        {
+            return await Db.Professors
+                .Where(p => p.DepartmentId == departmentId)
+                .ToListAsync();
+        }
     }
 }
