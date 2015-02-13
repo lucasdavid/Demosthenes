d37616c73def93aa9ec3bf7b6bb73cf9bcac78f3
Lucas David <lucas.david@drexel.edu>
2015-02-13 02:00:55 -0200

    Update front-end ClassesController

--- Demosthenes/Scripts/app/classes/classes-controller.js
+++ Demosthenes/Scripts/app/classes/classes-controller.js
@@ -1,12 +1,33 @@
 ﻿'use strict';
 
-app.controller('ClassesController', ['$scope', 'Validator', 'resolvedClasses', 'Classes',
-    'resolvedCourses', 'resolvedProfessors',
-    function ($scope, Validator, resolvedClasses, Classes, resolvedCourses, resolvedProfessors) {
+app.controller('ClassesController',
+    ['$scope', 'Validator',
+    'resolvedClasses', 'resolvedCourses', 'resolvedProfessors',
+    'Classes', 'Calendar',
+    
+    function ($scope, Validator, resolvedClasses, resolvedCourses, resolvedProfessors, Classes, Calendar) {
 
         $scope.classes    = resolvedClasses;
         $scope.courses    = resolvedCourses;
         $scope.professors = resolvedProfessors;
+        $scope.terms      = Calendar.terms();
+        $scope.years      = Calendar.years();
+
+        function getItemById(array, id) {
+            for (var i = 0; i < array.length; i++) {
+                if (array[i]["Id"] === id) {
+                    return array[i];
+                }
+            }
+
+            return null;
+        }
+        function getCourseById(id) {
+            return getItemById($scope.courses, id);
+        }
+        function getProfessorById(id) {
+            return getItemById($scope.professors, id);
+        }
 
         $scope.create = function () {
             Classes.save($scope.newClass,
@@ -14,8 +35,9 @@ app.controller('ClassesController', ['$scope', 'Validator', 'resolvedClasses', '
                 toastr.success('Class #' + _class.Id + ' saved!', 'Success!');
                 $scope.clear();
 
+                _class.Course    = getCourseById(_class.CourseId);
+                _class.Professor = getProfessorById(_class.ProfessorId);
                 $scope.classes.push(_class);
-                console.log(_class);
             },
             function (data) {
                 console.log(data);
