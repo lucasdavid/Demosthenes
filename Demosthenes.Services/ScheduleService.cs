d5275614ca3ff6bd823f28acc94851c52e8eece0
Lucas David <lucas.david@drexel.edu>
2015-02-13 01:54:26 -0200

    Add AllOrdered method to ScheduleService

--- Demosthenes.Services/ScheduleService.cs
+++ Demosthenes.Services/ScheduleService.cs
@@ -1,11 +1,23 @@
 ﻿using Demosthenes.Core;
 using Demosthenes.Data;
 using Demosthenes.Services.Infrastructure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data.Entity;
 
 namespace Demosthenes.Services
 {
     public class ScheduleService : Service<Schedule>
     {
         public ScheduleService(DemosthenesContext db) : base(db) { }
+
+        public async Task<ICollection<Schedule>> AllOrdered()
+        {
+            return await Db.Schedules
+                .OrderBy(s => s.DayOfWeek)
+                .ThenBy(s => s.TimeStarted)
+                .ToListAsync();
+        }
     }
 }
